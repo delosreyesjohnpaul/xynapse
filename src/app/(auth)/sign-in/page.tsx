6875ab9cc56653b933c300742e98d44c727f25f7
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-13 23:17:27 +0800

    signin

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { SignInCard } from "@/features/auth/components/sign-in-card";
 
 const SignInPage = () => {
--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,10 +1,15 @@
+import { FcGoogle} from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
 import { DottedSeparator } from "@/components/dotted-separator"
+import { Button } from "@/components/ui/button"
 import {
     Card,
     CardContent,
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
 
 export const SignInCard = () => {
     return (
@@ -17,6 +22,57 @@ export const SignInCard = () => {
             <div className="px-7">
                 <DottedSeparator/>
             </div>
+            <CardContent className="p-7">
+                <form className="space-y-4">
+                    <Input
+                        required
+                        type="email"
+                        value={""}
+                        onChange={() => {}}
+                        placeholder="Enter email address"
+                        disabled={false}
+                    />
+
+                    <Input
+                        required
+                        type="password"
+                        value={""}
+                        onChange={() => {}}
+                        placeholder="Enter password"
+                        disabled={false}
+                        min={8}
+                        max={256}
+                    />
+
+                    <Button disabled={false} size="lg" className="w-full">
+                        Login
+                    </Button>
+                </form>
+            </CardContent>
+            <div className="px-7">
+                <DottedSeparator/>
+            </div>
+            <CardContent className="p-7 flex flex-col gap-y-4">
+                <Button 
+                    disabled={false}
+                    variant="secondary"
+                    size="lg"
+                    className="w-full"
+                >
+                    <FcGoogle className="mr-2 size-5"/>
+                    Login with Google
+                </Button>
+
+                <Button 
+                    disabled={false}
+                    variant="secondary"
+                    size="lg"
+                    className="w-full"
+                >
+                    <FaGithub className="mr-2 size-5"/>
+                    Login with Github
+                </Button>
+            </CardContent>
        </Card>
     )
 }
