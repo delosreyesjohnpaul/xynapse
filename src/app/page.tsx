74e949abb7064c995fcdfdba132098219fa32bd2
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-13 21:03:17 +0800

    bug hu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,14 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button>cliahg</Button>
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">teritrary</Button>
+
     </div>
   )
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,26 +9,28 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+        primary:
+          "bg-gradient-to-b from-blue-600 to-blue-700 text-primary-foreground hover:from-blue-700 hover:to-blue-700",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "bg-gradient-to-b from-amber-600 to-amber-700 text-destructive-foreground hover:from-amber-700 hover:to-amber-700",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-white text-black hover:bg-neutral-100",
+        ghost: "border-transparent shadow-none hover:bg-accent hover:text-accent-foreground",
+        muted: "bg-neutral-200 text-neutral-600 hover:bg-neutral-200/80",
+        teritary: "bg-blue-100 text-blue-600 border-transparent hover:bg-blue-200 shadow-none"
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        default: "h-10 px-4 py-2",
+        sm: "h-8 rounded-md px-3",
+        xs: "h-7 rounded-md px-2 text-xs",
+        lg: "h-12 rounded-md px-8",
+        icon: "h-8 w-8",
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "primary",
       size: "default",
     },
   }
