1a8fe0d1cb0bd9cb7afbe1c4750b63dc0e15bbfa
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-16 00:11:03 +0800

    middleware

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -3,11 +3,18 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { loginSchema, registerSchema } from "../schemas";
 import { createAdminClient } from "@/lib/appwrite";
-import { ID } from "node-appwrite";
+import { Account, ID } from "node-appwrite";
 import { deleteCookie, setCookie } from "hono/cookie";
 import { AUTH_COOKIE } from "../constants";
+import { sessionMiddleware } from "@/lib/session-middleware";
 
 const app = new Hono()
+    .get("/current", sessionMiddleware, (c) => {
+        const user = c.get("user");
+
+        return c.json({data: user});
+    })
+    
     .post("/login", zValidator("json", loginSchema), async (c) => {
         const { email, password } = c.req.valid("json"); 
 
@@ -55,8 +62,11 @@ const app = new Hono()
         return c.json({ succes: true });
     })
 
-    .post("logout", (c) => {
+    .post("logout", sessionMiddleware, async (c) => {
+        const account = c.get("account");
+
         deleteCookie(c, AUTH_COOKIE);
+        await account.deleteSession("current");
 
         return c.json({ success: true });
     })
