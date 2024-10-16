dd93502a73248a16ed7d15e4bcd36d96bb284af0
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-16 14:14:43 +0800

    mobilesidebar

--- src/components/mobile-sidebar.tsx
+++ src/components/mobile-sidebar.tsx
@@ -1,7 +1,31 @@
 "use client";
 
+import { icons, MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Sidebar } from "./sidebar";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+
 export const MobileSidebar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const pathname = usePathname();
+
+    useEffect(() => {
+        setIsOpen(false);
+
+    }, [pathname]);
+
     return (
-        <
+        <Sheet modal={false} open={isOpen} onOpenChange={setIsOpen}>
+            <SheetTrigger asChild>
+                <Button variant="secondary" className="lg:hidden">
+                    <MenuIcon className="size-4 text-neutral-500"/>
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0">
+                <Sidebar/>
+            </SheetContent>
+        </Sheet>
     )
 } 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,15 @@
 import { UserButton } from "@/features/auth/components/user-button";
+import { MobileSidebar } from "./mobile-sidebar";
 
 export const Navbar = () => {
-    
+
     return (
        <nav className="pt-4 px-6 flex items-center justify-between">
         <div className="flex-col hidden lg:flex ">
             <h1 className="text-2xl font-semibold">Home</h1>
             <p className="text-muted-foreground ">Track and manage all your projects and tasks in one place.</p>
         </div>
+        <MobileSidebar/>
         <UserButton/>
        </nav>
     );
