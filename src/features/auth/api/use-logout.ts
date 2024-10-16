aa9ae085059140e0ddcb520b9b264fa2ee164660
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-17 00:34:35 +0800

    bugss

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -28,6 +28,7 @@ export const useLogout = () => {
             toast.success("Logged out")
             router.refresh();
             queryClient.invalidateQueries({ queryKey: ["current"]});
+            queryClient.invalidateQueries({queryKey: ["workspaces"]});
         },
 
         onError: () => {
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,16 +2,34 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
-import { ID } from "node-appwrite";
+import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { ID, Query } from "node-appwrite";
+import { MemberRole } from "@/features/members/types";
 
 const app = new Hono()
     .get("/", sessionMiddleware, async (c) => {
+        const user = c.get("user");
         const databases = c.get("databases");
+
+        const members = await databases.listDocuments(
+            DATABASE_ID,
+            MEMBERS_ID,
+            [Query.equal("userId", user.$id)]
+        );
+
+        if (members.total === 0) {
+            return c.json({ data: { documents: [], total: 0}});
+        }
+
+        const workspaceIds = members.documents.map((member) => member.workspaceId)
 
         const workspaces = await databases.listDocuments(
             DATABASE_ID,
             WORKSPACES_ID,
+            [
+                Query.orderDesc("$createdAt"),
+                Query.contains("$id", workspaceIds)
+            ],
         )
 
         return c.json({ data: workspaces });
@@ -54,6 +72,17 @@ const app = new Hono()
                     imageUrl: uploadedImageUrl,
                 },
             );
+
+            await databases.createDocument(
+                DATABASE_ID,
+                MEMBERS_ID,
+                ID.unique(),
+                {
+                    userId: user.$id,
+                    workspaceId: workspace.$id,
+                    role: MemberRole.ADMIN,
+                },
+            )
             
             return c.json({ data: workspace });
         }
