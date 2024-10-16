9fbcb1f84d5ba152ec8e777cab6846c2b041eda3
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-16 20:21:13 +0800

    buggshit

--- src/features/workspaces/schemas.ts
+++ src/features/workspaces/schemas.ts
@@ -2,4 +2,9 @@ import { z } from "zod";
 
 export const createWorkspaceSchema = z.object({
     name: z.string().trim().min(1, "Required"),
+    imageUrl: z.union([
+        z.instanceof(File),
+        z.string().transform((value) => value === "" ? undefined : value),
+    ])
+    .optional(),  
 });
