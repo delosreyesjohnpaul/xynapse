1303f9e5558cd5163d45413765294d92483e31d9
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-18 00:45:49 +0800

    redirect

--- src/app/(standalone)/workspaces/create/page.tsx
+++ src/app/(standalone)/workspaces/create/page.tsx
@@ -1,6 +1,12 @@
+import { getCurrent } from "@/features/auth/actions";
 import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
+import { redirect } from "next/navigation";
 
-const WorkspaceCreatePage = () => {
+const WorkspaceCreatePage = async () => {
+    const user = await getCurrent();
+
+    if(!user) redirect("/sign-in")
+
     return (
         <div className="w-full lg:max-w-xl">
             <CreateWorkspaceForm/>
