5966a513ef2d0da0da8189b503f6142c4deb54fa
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-18 01:20:09 +0800

    asgh

---
+++ src/features/members/utils.ts
@@ -0,0 +1,26 @@
+import { DATABASE_ID, MEMBERS_ID } from "@/config";
+import { Query, type Databases } from "node-appwrite";
+
+interface GetMemberProps {
+    databases: Databases;
+    workspaceId: string;
+    userId: string;
+}
+
+export const getMember = async ({
+    databases,
+    workspaceId,
+    userId,
+} : GetMemberProps) => {
+    const members = await databases.listDocuments(
+        DATABASE_ID,
+        MEMBERS_ID,
+        [
+            Query.equal("workspaceId", workspaceId),
+            Query.equal("userId", userId),
+
+        ],
+    );
+
+    return members.documents[0];
+};
