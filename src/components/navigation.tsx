ae24daecc1d78562ce337b96335c796f87501f12
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-18 02:27:40 +0800

    fullhreff

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import { cn } from "@/lib/utils";
-import { SettingsIcon, UsersIcon } from "lucide-react"
+import { SettingsIcon, UsersIcon } from "lucide-react";
 import Link from "next/link";
-import { GoCheckCircle, GoCheckCircleFill, GoHome, GoHomeFill } from "react-icons/go"
+import { GoCheckCircle, GoCheckCircleFill, GoHome, GoHomeFill } from "react-icons/go";
+
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { usePathname } from "next/navigation";
+
 
 const routes = [
     {
@@ -33,14 +39,18 @@ const routes = [
 ];
 
 export const Navigation = () => {
+    const workspaceId = useWorkspaceId();
+    const pathname = usePathname();
+
     return (
         <ul className="flex flex-col">
             {routes.map((item) => {
-                const isActive = false;
+                const fullHref = `/workspaces/${workspaceId}${item.href}`
+                const isActive = pathname === fullHref;
                 const Icon = isActive ? item.activeIcon : item.icon;
 
                 return (
-                    <Link key={item.href} href={item.href}>
+                    <Link key={item.href} href={fullHref}>
                         <div className={cn(
                             "flex items-center gap-2.5 p-2.5 rounded-md font-medium hover:text-primary transition text-neutral-500",
                             isActive && "bg-white shadow-sm hover:opacity-100 text-primary"
