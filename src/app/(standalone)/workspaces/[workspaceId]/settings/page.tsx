b46de8e0faca5a8c14d80fdf9a38aab594b70874
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-19 12:18:11 +0800

    ahga asl a

--- src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -24,7 +24,7 @@ const WorkspaceIdSettingsPage = async ({
     }
 
     return (
-        <div>
+        <div className="w-full lg:max-w-xl ">
             <EditWorkspaceForm initialValues={initialValues}/>
         </div>
     );
--- src/features/workspaces/components/edit-workspace-form.tsx
+++ src/features/workspaces/components/edit-workspace-form.tsx
@@ -25,13 +25,13 @@ import {
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useCreateWorkspace } from "../api/use-create-workspace";
 import Image from "next/image";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import { ImageIcon } from "lucide-react";
+import { ArrowLeft, ImageIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Workspace } from "../types";
+import { useUpdateWorkspace } from "../api/use-update-workspace";
 
 interface EditWorkspaceFormProps {
     onCancel?: () => void;
@@ -40,7 +40,7 @@ interface EditWorkspaceFormProps {
 
 export const EditWorkspaceForm = ({ onCancel, initialValues } : EditWorkspaceFormProps) => {
     const router = useRouter();
-    const { mutate, isPending } = useCreateWorkspace();
+    const { mutate, isPending } = useUpdateWorkspace();
 
     const inputRef = useRef<HTMLInputElement>(null);
 
@@ -78,7 +78,11 @@ export const EditWorkspaceForm = ({ onCancel, initialValues } : EditWorkspaceFor
 
     return (
         <Card className="w-full h-full border-none shadow-none">
-            <CardHeader className="flex p-7">
+            <CardHeader className="flex flex-row items-center gap-x-4 p-7 space-y-0">
+                <Button size="sm" variant="secondary" onClick={onCancel ? onCancel : () => router.push(`/workspaces/${initialValues.$id}`)}>
+                    <ArrowLeft className="size-4 mr-2"/>
+                    Back
+                </Button>
                 <CardTitle className="text-xl font-bold">
                     {initialValues.name}
                 </CardTitle>
@@ -182,7 +186,7 @@ export const EditWorkspaceForm = ({ onCancel, initialValues } : EditWorkspaceFor
                                 size="lg"
                                 disabled={isPending}
                             >
-                                Create Workspace
+                                Save Changes
                             </Button>
                         </div>
                     </form>
