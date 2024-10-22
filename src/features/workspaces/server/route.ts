2e3ed0100123d70100a236ff1dd705ad9aa51073
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-22 16:17:51 +0800

    useResetInviteCode

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -182,4 +182,39 @@ const app = new Hono()
         }
     )
 
+    .post(
+        "/:workspaceId/reset-invite-code",
+        sessionMiddleware,
+        async (c) => {
+            const databases = c.get("databases");
+            const user = c.get("user");
+
+            const { workspaceId } = c.req.param();
+
+            const member = await getMember({
+                databases,
+                workspaceId,
+                userId: user.$id,
+            });
+
+            if (!member || member.role !== MemberRole.ADMIN) {
+                return c.json({
+                    error: "Unauthorized"
+                }, 401);
+            }
+
+            const workspace = await databases.updateDocument(
+                DATABASE_ID,
+                WORKSPACES_ID,
+                workspaceId,
+                {
+                    inviteCode: generateInviteCode(6),
+                },
+            );
+
+            return c.json({ data: workspace });
+        }
+    )
+
+
 export default app;
