51568d1e23b05eb18199a6ccd1757e62a4f6e79e
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-22 14:16:50 +0800

    createSessionClient

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -7,6 +7,32 @@ import {
     Users,
     Databases,
 } from "node-appwrite";
+import { cookies } from "next/headers";
+import { AUTH_COOKIE } from "@/features/auth/constants";
+
+export async function createSessionClient() {
+    const client = new Client()
+        .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+        .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+
+    const session = await cookies().get(AUTH_COOKIE);
+
+    if (!session || !session.value) {
+        throw new Error("Unauthorized");
+    }
+
+    client.setSession(session.value);
+
+    return {
+        get account() {
+            return new Account(client);
+        },
+
+        get databases() {
+            return new Databases(client);
+        }
+    };
+}
 
 export async function createAdminClient() {
     const client = new Client()
