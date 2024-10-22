2b1ced8e6a163c7c1a49b4ecee01001b64bb513e
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-22 21:38:01 +0800

    useJoinWorkspace

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -7,6 +7,8 @@ import { ID, Query } from "node-appwrite";
 import { MemberRole } from "@/features/members/types";
 import { generateInviteCode } from "@/lib/utils";
 import { getMember } from "@/features/members/utils";
+import { z } from "zod";
+import { Workspace } from "../types";
 
 const app = new Hono()
     .get("/", sessionMiddleware, async (c) => {
@@ -215,6 +217,52 @@ const app = new Hono()
             return c.json({ data: workspace });
         }
     )
+
+    .post(
+        "/:workspaceId/join",
+        sessionMiddleware,
+        zValidator("json", z.object({ code: z.string() })),
+        async (c) => {
+            const { workspaceId } = c.req.param();
+            const { code } = c.req.valid("json");
+
+            const databases = c.get("databases");
+            const user = c.get("user");
+
+            const member = await getMember({
+                databases,
+                workspaceId,
+                userId: user.$id,  
+            });
+
+            if (member) {
+                return c.json({ error: "Already a member" }, 400);
+            }
+
+            const workspace = await databases.getDocument<Workspace>(
+                DATABASE_ID,
+                WORKSPACES_ID,
+                workspaceId
+            );
+
+            if (workspace.inviteCode !== code) {
+                return c.json({ error: "Invalid invite code" }, 400);
+            }
+
+            await databases.createDocument(
+                DATABASE_ID,
+                WORKSPACES_ID,
+                ID.unique(),
+                {
+                    workspaceId,
+                    userId: user.$id,
+                    role: MemberRole.MEMBER,
+                },
+            );
+            return c.json({ data: workspace });
+
+        }
+    );
 
 
 export default app;
