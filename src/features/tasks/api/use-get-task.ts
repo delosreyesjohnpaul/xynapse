992e84239ac68799ec3783bf0316f65c6b447ae1
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 02:14:22 +0800

    sahs

---
+++ src/features/tasks/api/use-get-task.ts
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { client } from "@/lib/rpc";
+
+interface UseGetProjectsProps {
+    workspaceId: string;
+}
+
+export const useGetProjects = ({
+    workspaceId,
+} : UseGetProjectsProps) => {
+    const query = useQuery({
+        queryKey: ["projects", workspaceId],
+        queryFn: async () => {
+            const response = await client.api.projects.$get({
+                query: { workspaceId },
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch projects");
+            }
+
+            const { data } = await response.json();
+
+            return data;
+        }
+    })
+
+    return query;
+}
