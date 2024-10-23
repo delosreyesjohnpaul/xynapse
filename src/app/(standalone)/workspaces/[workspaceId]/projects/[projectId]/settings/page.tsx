c90beb680351d2c8929323b47b2f7aa3ba3ff83e
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-23 20:48:31 +0800

    akjhsfh

--- src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
@@ -1,6 +1,25 @@
-const ProjecIdSettingsPage = () => {
+import { getCurrent } from "@/features/auth/queries";
+import { getProject } from "@/features/projects/queries";
+import { redirect } from "next/navigation";
+
+interface ProjecIdSettingsPageProps {
+    params: {
+        projectId: string;
+    };
+};
+
+const ProjecIdSettingsPage = async ({
+    params,
+} : ProjecIdSettingsPageProps) => {
+    const user = await getCurrent();
+    if(!user) redirect("/sign-in");
+    
+    const initialValues = await getProject({
+        projectId: params.projectId,
+    });
+
     return (
-        <div>
+        <div className="w-full lg:max-w-xl ">
             settingspage
         </div>
     )
