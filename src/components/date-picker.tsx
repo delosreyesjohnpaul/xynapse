0e8fa780426fd17c5410d3da96dec8c3abd1e886
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 03:00:29 +0800

    alshfah

--- src/components/date-picker.tsx
+++ src/components/date-picker.tsx
@@ -13,3 +13,44 @@ import {
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover";
+
+interface DatePickerProps {
+    value: Date | undefined;
+    onChange: (date: Date) => void;
+    className?: string;
+    placeholder?: string;
+};
+
+export const DatePicker = ({
+    value,
+    onChange,
+    className,
+    placeholder = "Select date",
+} : DatePickerProps) => {
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    size="lg"
+                    className={cn(
+                        "w-full justify-start text-left font-normal px-3",
+                        !value && "text-muted-foreground",
+                        className
+                    )}
+                >
+                    <CalendarIcon className="mr-2 h-4 w-4"/>
+                    {value ? format(value, "PPP") : <span>{placeholder}</span>}
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent>
+                <Calendar
+                    mode="single"
+                    selected={value}
+                    onSelect={(date) => onChange(date as Date)}
+                    initialFocus
+                />
+            </PopoverContent>
+        </Popover>
+    )
+}
