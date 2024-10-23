b0f490e830d244b810398976db4709b7bea04f59
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-23 19:18:13 +0800

    oasgdhash

---
+++ src/features/projects/queries.ts
@@ -0,0 +1,38 @@
+import { createSessionClient } from "@/lib/appwrite";
+import { getMember } from "../members/utils";
+import { DATABASE_ID, PROJECTS_ID } from "@/config";
+import { Project } from "./types";
+
+interface GetProjectProps {
+    projectId: string;
+}
+
+export const getProject = async ({ projectId }: GetProjectProps) => {
+
+    try {
+        const {  databases, account }  = await createSessionClient();
+
+        const user = await account.get();
+
+        const project = await databases.getDocument<Project>(
+            DATABASE_ID,
+            PROJECTS_ID,
+            projectId,
+        )
+
+        const member = await getMember({
+            databases,
+            userId: user.$id,
+            workspaceId: project.workspaceId,
+        });
+
+        if(!member) {
+            return null;
+        }
+
+        return project;
+
+    }catch{
+        return null;
+    }
+}
