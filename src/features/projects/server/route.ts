7a5361f85870dbd166c3afd7c3e5e4633571171d
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-23 15:11:57 +0800

    alkjgoag

--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -1,12 +1,67 @@
-import { DATABASE_ID, PROJECTS_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, PROJECTS_ID } from "@/config";
 import { getMember } from "@/features/members/utils";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { z } from "zod";
+import { createProjectSchema } from "../schema";
 
 const app = new Hono()
+    .post(
+        "/",
+        sessionMiddleware,
+        zValidator("form", createProjectSchema),
+        async (c) => {
+            const databases = c.get("databases");
+            const storage = c.get("storage");
+            const user = c.get("user");
+
+            const { name, image, workspaceId } = c.req.valid("form");
+
+            const member = await getMember({
+                databases,
+                workspaceId,
+                userId: user.$id
+            });
+
+            if(!member) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+            
+            let uploadedImageUrl: string | undefined;
+
+            if (image instanceof File ) {
+                const file = await storage.createFile(
+                    IMAGES_BUCKET_ID,
+                    ID.unique(),
+                    image,
+                );
+
+                const arrayBuffer = await storage.getFilePreview(
+                    IMAGES_BUCKET_ID,
+                    file.$id,
+                );
+
+                uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
+            }
+            
+
+            const project = await databases.createDocument(
+                DATABASE_ID,
+                PROJECTS_ID,
+                ID.unique(),
+                {
+                    name,
+                    userId: user.$id,
+                    imageUrl: uploadedImageUrl,
+                    workspaceId
+                },
+            );
+            
+            return c.json({ data: project });
+        }
+    )
     .get(
         "/",
         sessionMiddleware,
