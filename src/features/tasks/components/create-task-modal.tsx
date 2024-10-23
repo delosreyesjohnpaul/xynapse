8124c14bfb8276a1adbbc9e9c6603d955728cd0a
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 02:03:53 +0800

    asfhah

--- src/features/tasks/components/create-task-modal.tsx
+++ src/features/tasks/components/create-task-modal.tsx
@@ -2,15 +2,14 @@
 
 import { ResponsiveModal } from "@/components/responsive-modal";
 import { useCreateTasktModal } from "../hooks/use-create-task-modal";
+import { CreateTaskFormWrapper } from "./create-task-form-wrapper";
 
 export const CreateTaskModal = () => {
-    const { isOpen, setIsOpen } = useCreateTasktModal();
+    const { isOpen, setIsOpen, close} = useCreateTasktModal();
     
     return (
         <ResponsiveModal open={isOpen} onOpenChange={setIsOpen}>
-            <div>
-                TODO: Task form
-            </div>
+            <CreateTaskFormWrapper onCancel={close}/>
         </ResponsiveModal>
     );
 };
