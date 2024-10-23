e5121b07e8bc31cdcad87591229bf56f8a3130e1
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-23 19:41:08 +0800

    ErrorPage

---
+++ src/app/error.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { AlertTriangle } from "lucide-react";
+import Link from "next/link";
+
+const ErrorPage = () => {
+    return (
+        <div className="h-screen flex flex-col gap-y-2 items-center justify-center">
+            <AlertTriangle/>
+            <p className="text-sm text-muted-foreground">Something went wrong</p>
+            <Button variant="secondary" asChild>
+                <Link href="/">
+                    Back to home
+                </Link>
+            </Button>
+        </div>
+    )
+}
+
+export default ErrorPage;
