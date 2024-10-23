fe491402ee55aa84e5928dc8d509905ce702b9d3
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-23 20:17:35 +0800

    update project

--- src/features/projects/schemas.ts
+++ src/features/projects/schemas.ts
@@ -9,3 +9,12 @@ export const createProjectSchema = z.object({
     .optional(),
     workspaceId: z.string(),
 });
+
+export const updateProjectSchema = z.object({
+    name: z.string().trim().min(1, "Minimum 1 character required").optional(),
+    image: z.union([
+        z.instanceof(File),
+        z.string().transform((value) => value === "" ? undefined : value),
+    ])
+    .optional(),
+});
--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -5,7 +5,8 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { ID, Query } from "node-appwrite";
 import { z } from "zod";
-import { createProjectSchema } from "../schemas";
+import { createProjectSchema, updateProjectSchema } from "../schemas";
+import { Project } from "../types";
 
 const app = new Hono()
     .post(
@@ -95,6 +96,67 @@ const app = new Hono()
             );
 
             return c.json({ data: projects });
+        }
+    )
+
+    .patch(
+        "/:projectId",
+        sessionMiddleware,
+        zValidator("form", updateProjectSchema),
+        async (c) => {
+            const databases = c.get("databases");
+            const storage = c.get("storage");
+            const user = c.get("user");
+
+            const { projectId } = c.req.param();
+            const { name, image } = c.req.valid("form");
+
+            const existingProject = await databases.getDocument<Project>(
+                DATABASE_ID,
+                PROJECTS_ID,
+                projectId
+            );
+
+            const member = await getMember({
+                databases,
+                workspaceId: existingProject.workspaceId,
+                userId: user.$id,
+            });
+
+            if (!member) {
+                return c.json({ error: "Unauthorized"}, 401);
+            }
+
+            let uploadedImageUrl: string | undefined;
+
+            if (image instanceof File ) {
+                const file = await storage.createFile(
+                    IMAGES_BUCKET_ID,
+                    ID.unique(),
+                    image,
+                );
+
+                const arrayBuffer = await storage.getFilePreview(
+                    IMAGES_BUCKET_ID,
+                    file.$id,
+                );
+
+                uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
+            } else {
+                uploadedImageUrl = image;
+            }
+
+            const project = await databases.updateDocument(
+                DATABASE_ID,
+                PROJECTS_ID,
+                projectId,
+                {
+                    name,
+                    imageUrl: uploadedImageUrl,
+                }
+            );
+
+            return c.json({ data: project });
         }
     )
 
