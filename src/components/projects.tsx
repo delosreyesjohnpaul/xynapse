5951f39b664801826aeb7e6e044ebfe39584666f
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-23 14:49:41 +0800

    ahgahsshkdja

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,14 +1,45 @@
 "use client";
 
+import { useGetProjects } from "@/features/projects/api/use-get-project";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import Link from "next/link";
 import { RiAddCircleFill } from "react-icons/ri";
 
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
 export const Projects = () => {
+    const projectId = null; //todoaogsaasjgh
+
+    const pathname = usePathname();
+    const workspaceId = useWorkspaceId();
+    const { data } = useGetProjects({
+        workspaceId,
+    });
+
     return (
         <div className="flex flex-col gap-y-2">
             <div className="flex items-center justify-between">
                 <p className="text-xs uppercase text-neutral-500">Projects</p>
                 <RiAddCircleFill onClick={() =>{}} className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"/>
             </div>
+            {data?.documents.map((project) => {
+                const href = `/workspaces/${workspaceId}/projects/${projectId}`;
+                const isActive = pathname === href;
+
+                return (
+                    <Link href={href} key={project.$id}>
+                        <div
+                            className={cn(
+                                "flex items-center gap-2.5 p-2.5 rounded-md hover:opacity-75 transition cursor-pointer text-neutral-500",
+                                isActive && "bg-white shadow-sm hover:opacity-100 text-primary"
+                            )}
+                        >
+                            <span className="truncate">{project.name}</span>
+                        </div>
+                    </Link>
+                )
+            })}
         </div>
     );
 };
