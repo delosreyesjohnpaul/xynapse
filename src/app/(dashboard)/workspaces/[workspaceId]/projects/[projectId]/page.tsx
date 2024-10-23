a2ecd030e90d225b55dd8746aa9f3971431926fc
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-23 19:34:21 +0800

    askhjgh

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@/components/ui/button";
 import { getCurrent } from "@/features/auth/queries";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { getProject } from "@/features/projects/queries";
+import { PencilIcon } from "lucide-react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 interface ProjectIdPageProps {
@@ -16,9 +20,35 @@ const ProjectIdPage = async ({
         projectId: params.projectId,
     })
 
+    if (!initialValues) {
+        throw new Error("Project not found");
+    }
+
     return (
         <div className="flex flex-col gap-y-4">
-            <div></div>
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-x-2 ">
+                    <ProjectAvatar 
+                        name={initialValues.name}
+                        image={initialValues.imageUrl}
+                        className="size-8"
+                    />
+                    <p className="text-lg font-semibold">{initialValues.name}</p>
+                </div>
+                <div>
+                    <Button 
+                      variant="secondary"
+                      size="sm"
+                      asChild  
+                    >
+                        <Link href={`/workspaces/${initialValues.workspaceId}/projects/${initialValues.$id}/settings`}>
+                            <PencilIcon className="size-4 mr-2"/>
+                            Edit Project
+                        </Link>
+                    </Button>
+                </div>
+            </div>
+
         </div>
     );
 };
