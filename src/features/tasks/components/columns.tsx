4acb5cbcf1ef522e7b1cc0b8e87574a6410c6dee
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 16:41:06 +0800

    asflfja

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -5,6 +5,7 @@ import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown } from "lucide-react";
 import { Task } from "../types";
 import { Button } from "@/components/ui/button";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 
 export const columns: ColumnDef<Task>[] = [
     {
@@ -19,6 +20,42 @@ export const columns: ColumnDef<Task>[] = [
                 <ArrowUpDown className="ml-2 h-4 w-4" />
               </Button>
             )
-          },
+        },
+        cell: ({ row }) => {
+            const name = row.original.name;
+
+            return <p className="line-clamp-1">{name}</p>
+        },
+
     },
+
+    {
+        accessorKey: "project",
+        header: ({ column }) => {
+            return (
+                <Button
+                  variant="ghost"
+                  onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                  Project
+                  <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+        cell: ({ row }) => {
+            const project = row.original.project;
+
+            return (
+                <div className="flex items-center gap-x-2 text-sm font-medium">
+                    <ProjectAvatar
+                        className="size-6"
+                        name={project.name}
+                        image={project.imageUrl}
+                    />
+
+                    <p className="line-clamp-1">{project.name}</p>
+                </div>
+            )
+        }
+    }
 ];
