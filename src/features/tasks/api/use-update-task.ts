046edf88ff4a7d44129d1f2a5b42ac0ceda3d31a
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 22:22:25 +0800

    useupdate task

--- src/features/tasks/api/use-update-task.ts
+++ src/features/tasks/api/use-update-task.ts
@@ -5,11 +5,14 @@ import { InferRequestType, InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
-type ResponseType = InferResponseType<typeof client.api.tasks["$post"], 200>;
-type RequestType = InferRequestType<typeof client.api.tasks["$post"]>;
+type ResponseType = InferResponseType<typeof client.api.tasks[":taskId"]["$patch"], 200>;
+type RequestType = InferRequestType<typeof client.api.tasks[":taskId"]["$patch"]>;
 
-export const useCreateTask = () => {
+export const useUpdateTask = () => {
+    const router = useRouter();
+
     const queryClient = useQueryClient();
 
     const mutation = useMutation<
@@ -17,21 +20,24 @@ export const useCreateTask = () => {
         Error,
         RequestType
     >({
-        mutationFn: async ({ json }) => {
-            const response = await client.api.tasks["$post"]({ json });
+        mutationFn: async ({ json, param }) => {
+            const response = await client.api.tasks[":taskId"]["$patch"]({ json, param });
 
             if (!response.ok) {
-                throw new Error("Failed to create task");
+                throw new Error("Failed to update task");
             }
 
             return await response.json();
         },
-        onSuccess: () => {
-            toast.success("Task created");
+        onSuccess: ({ data }) => {
+            toast.success("Task updated");
+
+            router.refresh();
             queryClient.invalidateQueries({ queryKey: ["tasks"]});
+            queryClient.invalidateQueries({ queryKey: ["task", data.$id]});
         },
         onError: () => {
-            toast.error("Failed to create task");
+            toast.error("Failed to update task");
         }
     })
 
