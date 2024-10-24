c2cc071e10c30fe941208c6630fbd577d3a6ba60
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 14:41:36 +0800

    oasfhah

--- src/features/tasks/components/data-filters.tsx
+++ src/features/tasks/components/data-filters.tsx
@@ -12,6 +12,8 @@ import {
 } from "@/components/ui/select";
 
 import { DatePicker } from "@/components/date-picker";
+import { ListCheckIcon } from "lucide-react";
+import { TaskStatus } from "../types";
 
 interface DataFiltersProps {
     hideProjectFilters?: boolean;
@@ -39,7 +41,42 @@ export const DataFilters = ({ hideProjectFilters } : DataFiltersProps) => {
 
     return (
         <div className="flex flex-col lg:flex-row gap-2">
-            
+            <Select
+             defaultValue={undefined}
+             onValueChange={() => {}}
+            >
+                <SelectTrigger className="w-full lg:w-auto h-8">
+                    <div className="flex items-center pr-2">
+                        <ListCheckIcon className="size-4 mr-2"/>
+                        <SelectValue placeholder="All Statuses"/>
+                    </div>
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectItem value="all">All Statuses</SelectItem>
+                    <SelectSeparator/>
+
+                    <SelectItem value={TaskStatus.BACKLOG}>
+                        Backlog
+                    </SelectItem>
+
+                    <SelectItem value={TaskStatus.IN_PROGRESS}>
+                        In Progress
+                    </SelectItem>
+
+                    <SelectItem value={TaskStatus.IN_REVIEW}>
+                        In Review
+                    </SelectItem>
+
+                    <SelectItem value={TaskStatus.TODO}>
+                        Todo
+                    </SelectItem>
+
+                    <SelectItem value={TaskStatus.DONE}>
+                        Done
+                    </SelectItem>
+
+                </SelectContent>
+            </Select>
         </div>
     )
 
