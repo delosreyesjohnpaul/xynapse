26326175c7a67c4be4d87b07c8499113d8055db2
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 17:05:15 +0800

    asfhaj

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { MemberAvatar } from "@/features/members/components/member-avatar";
 import { TaskDate } from "./task-date";
+import { Badge } from "@/components/ui/badge";
 
 export const columns: ColumnDef<Task>[] = [
     {
@@ -108,6 +109,26 @@ export const columns: ColumnDef<Task>[] = [
             const dueDate = row.original.dueDate;
 
             return <TaskDate value={dueDate} />
+        }
+    },
+
+    {
+        accessorKey: "status",
+        header: ({ column }) => {
+            return (
+                <Button
+                  variant="ghost"
+                  onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                Status
+                  <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+        cell: ({ row }) => {
+            const status = row.original.status;
+
+            return <Badge>{status}</Badge>
         }
     }
 ];
