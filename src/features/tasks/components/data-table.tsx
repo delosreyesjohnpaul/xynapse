3a46c277e570a2c96bc38e6af3af1e095fb95119
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 16:28:14 +0800

    rm :

--- src/features/tasks/components/data-table.tsx
+++ src/features/tasks/components/data-table.tsx
@@ -60,16 +60,6 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
-       <div className="flex items-center py-4">
-        <Input
-          placeholder="Filter tasks..."
-          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
-          onChange={(event) =>
-            table.getColumn("name")?.setFilterValue(event.target.value)
-          }
-          className="max-w-sm"
-        />
-      </div>
       <div className="rounded-md border">
         <Table>
           <TableHeader>
