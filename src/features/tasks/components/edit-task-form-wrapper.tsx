f53077f392636311ce86be7dd8d3a9e31de62113
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 23:13:49 +0800

    alsfk

--- src/features/tasks/components/edit-task-form-wrapper.tsx
+++ src/features/tasks/components/edit-task-form-wrapper.tsx
@@ -3,8 +3,8 @@ import { useGetMembers } from "@/features/members/api/use-get-members";
 import { useGetProjects } from "@/features/projects/api/use-get-project";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { Loader } from "lucide-react";
-import { CreateTaskForm } from "./create-task-form";
 import { useGetTask } from "../api/use-get-task";
+import { EditTaskForm } from "./edit-task-form";
 
 interface EditTaskFormWrapperProps {
     onCancel: () => void;
@@ -53,8 +53,9 @@ export const EditTaskFormWrapper = ({
     }
 
     return (
-        <CreateTaskForm
+        <EditTaskForm
             onCancel={onCancel}
+            initialValues={initialValues}
             projectOptions={projectOptions ?? []}
             memberOptions={memberOptions ?? []}
         />
