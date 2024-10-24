119e9842c7cf47bbb80a58148c7b2e0cfe2caa2d
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 14:49:44 +0800

    useTaskFilters

---
+++ src/features/tasks/hooks/use-task-filters.ts
@@ -0,0 +1,16 @@
+import {
+    parseAsString,
+    parseAsStringEnum,
+    useQueryStates,
+} from "nuqs";
+import { TaskStatus } from "../types";
+
+export const useTaskFilters = () => {
+    return useQueryStates({
+        projectId: parseAsString,
+        status: parseAsStringEnum(Object.values(TaskStatus)),
+        assigneeId: parseAsString,
+        search: parseAsString,
+        dueDate: parseAsString,
+    });
+}
