b9ad751abe13d348bd9c35822687273cd88f634a
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 22:16:45 +0800

    use create task

--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -231,4 +231,56 @@ const app = new Hono()
         }
     )
 
+    .patch(
+        "/:taskId",
+        sessionMiddleware,
+        zValidator("json", createTaskSchema.partial()),
+        async (c) => {
+            const user = c.get("user");
+            const databases = c.get("databases");
+            const {
+                name,
+                status,
+                description,
+                projectId,
+                dueDate,
+                assigneeId,
+            } = c.req.valid("json");
+
+            const { taskId } = c.req.param();
+
+            const existingTask = await databases.getDocument<Task>(
+                DATABASE_ID,
+                TASKS_ID,
+                taskId,
+            )
+
+            const member = await getMember({
+                databases,
+                workspaceId: existingTask.workspaceId,
+                userId: user.$id
+            });
+
+            if(!member) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const task = await databases.updateDocument<Task>(
+                DATABASE_ID,
+                TASKS_ID,
+                taskId,
+                {
+                    name,
+                    status,
+                    projectId,
+                    dueDate,
+                    assigneeId,
+                    description,
+                },
+            );
+
+            return c.json({ data: task });
+        }
+    )
+
 export default app;
