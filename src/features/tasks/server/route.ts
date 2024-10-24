4c44c24833811386d32057fd64d8674aa7c15f85
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 22:52:02 +0800

    alsfh

--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -283,4 +283,61 @@ const app = new Hono()
         }
     )
 
+    .get(
+        "/:taskId",
+        sessionMiddleware,
+        async (c) => {
+            const currentUser = c.get("user");
+            const databases = c.get("databases");
+            const { users } = await createAdminClient();
+
+            const { taskId } = c.req.param();
+
+            const task = await databases.getDocument<Task>(
+                DATABASE_ID,
+                TASKS_ID,
+                taskId,
+            );
+
+            const currentMember = await getMember({
+                databases,
+                workspaceId: task.workspaceId,
+                userId: currentUser.$id
+            });
+
+            if (!currentMember) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const project = await databases.getDocument<Project>(
+                DATABASE_ID,
+                PROJECTS_ID,
+                task.projectId
+            );
+
+            const member = await databases.getDocument(
+                DATABASE_ID,
+                MEMBERS_ID,
+                task.assigneeId
+            );
+
+            const user = await users.get(member.userId);
+
+            const assignee = {
+                ...member,
+                name: user.name,
+                email: user.email,
+            };
+
+            return c.json({
+                data: {
+                    ...task,
+                    project,
+                    assignee,
+                },
+            });
+
+        }
+    )
+
 export default app;
