0bf59b7929663061425da4619530d13c3e3d9863
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 15:46:55 +0800

    Task

--- src/features/tasks/types.ts
+++ src/features/tasks/types.ts
@@ -1,7 +1,18 @@
+import { Models } from "node-appwrite";
+
 export enum TaskStatus {
     BACKLOG = "BACKLOG",
     TODO= "TODO",
     IN_PROGRESS = "IN_PROGRESS",
     IN_REVIEW = "IN_REVIEW",
     DONE = "DONE"
+}
+
+export type Task = Models.Document & {
+    name: string;
+    status: TaskStatus;
+    assigneeId: string;
+    projectId: string;
+    position: number;
+    dueDate: string;
 }
