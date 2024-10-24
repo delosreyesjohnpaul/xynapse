c224e4aa0bbf04d8e2af88c3a20105ab1392da88
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 16:21:01 +0800

    edit header data table: :

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -1,11 +1,24 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+
+import { ArrowUpDown } from "lucide-react";
 import { Task } from "../types";
+import { Button } from "@/components/ui/button";
 
 export const columns: ColumnDef<Task>[] = [
     {
         accessorKey: "name",
-        header: "Task Name", 
+        header: ({ column }) => {
+            return (
+              <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+              >
+                Task Name
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+              </Button>
+            )
+          },
     },
 ];
