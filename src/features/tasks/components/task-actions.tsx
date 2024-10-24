b5ad302d12cde57482623fefbc116f54d9cb7419
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 17:43:32 +0800

    alahg

--- src/features/tasks/components/task-actions.tsx
+++ src/features/tasks/components/task-actions.tsx
@@ -4,7 +4,7 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ExternalLinkIcon } from "lucide-react";
+import { ExternalLinkIcon, PencilIcon, TrashIcon } from "lucide-react";
 
 interface TaskActionProps {
     id: string;
@@ -31,6 +31,33 @@ export const TaskAction = ({
                     >
                         <ExternalLinkIcon className="size-4 mr-2 stroke-2"/>
                         Task Details
+                    </DropdownMenuItem>
+                                        
+                    <DropdownMenuItem
+                        onClick={() => {}}
+                        disabled={false}
+                        className="font-medium p-[10px]"
+                    >
+                        <ExternalLinkIcon className="size-4 mr-2 stroke-2"/>
+                        Open Project
+                    </DropdownMenuItem>
+
+                    <DropdownMenuItem
+                        onClick={() => {}}
+                        disabled={false}
+                        className="font-medium p-[10px]"
+                    >
+                        <PencilIcon className="size-4 mr-2 stroke-2"/>
+                        Edit Task
+                    </DropdownMenuItem>
+                    
+                    <DropdownMenuItem
+                        onClick={() => {}}
+                        disabled={false}
+                        className="text-amber-700 focus:text-amber-700 font-medium p-[10px]"
+                    >
+                        <TrashIcon className="size-4 mr-2 stroke-2"/>
+                        Delete Task
                     </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
