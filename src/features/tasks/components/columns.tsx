5b93cf1dec64e9b0fafc025003c9a444f5e2b19f
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 17:01:16 +0800

    asklfa

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -7,6 +7,7 @@ import { Task } from "../types";
 import { Button } from "@/components/ui/button";
 import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { MemberAvatar } from "@/features/members/components/member-avatar";
+import { TaskDate } from "./task-date";
 
 export const columns: ColumnDef<Task>[] = [
     {
@@ -87,6 +88,26 @@ export const columns: ColumnDef<Task>[] = [
                     <p className="line-clamp-1">{assignee.name}</p>
                 </div>
             )
+        }
+    },
+
+    {
+        accessorKey: "dueDate",
+        header: ({ column }) => {
+            return (
+                <Button
+                  variant="ghost"
+                  onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                Due Date
+                  <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+        cell: ({ row }) => {
+            const dueDate = row.original.dueDate;
+
+            return <TaskDate value={dueDate} />
         }
     }
 ];
--- src/features/tasks/components/task-date.tsx
+++ src/features/tasks/components/task-date.tsx
@@ -11,4 +11,22 @@ export const TaskDate = ({ value, className } : TaskDateProps) => {
     const today = new Date();
     const endDate = new Date(value);
     const diffInDays = differenceInDays(endDate, today);
+
+    let textColor = "text-muted-foreground";
+
+    if (diffInDays <= 3) {
+        textColor = "text-red-500";
+    } else if (diffInDays <= 7) {
+        textColor = "text-orange-500";
+    } else if (diffInDays <= 14) {
+        textColor = "text-yellow-500";
+    }
+
+    return (
+        <div className={textColor}>
+            <span className={cn("truncate", className)}>
+                {format(value, "PPP")}
+            </span>
+        </div>
+    )
 }
