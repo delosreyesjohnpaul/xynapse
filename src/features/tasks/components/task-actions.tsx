35742da1e98fa40436abda38ba11fbb60bba5479
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-24 21:56:18 +0800

    aslkf

--- src/features/tasks/components/task-actions.tsx
+++ src/features/tasks/components/task-actions.tsx
@@ -4,7 +4,11 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+
+import { useConfirm } from "@/hooks/use-confirm";
 import { ExternalLinkIcon, PencilIcon, TrashIcon } from "lucide-react";
+
+import { useDeleteTask } from "../api/use-delete-task";
 
 interface TaskActionProps {
     id: string;
@@ -17,8 +21,25 @@ export const TaskAction = ({
     projectId,
     children,
 } : TaskActionProps) => {
+    const [ConfirmDialog, confirm] = useConfirm(
+        "Delete task",
+        "This action connot be undone.",
+        "destructive"
+    );
+
+    const { mutate, isPending} = useDeleteTask();
+
+    const onDelete = async () => {
+        const ok = await confirm();
+
+        if (!ok) return;
+
+        mutate({ param: { taskId: id }});
+    }
+
     return(
         <div className="flex justify-end">
+            <ConfirmDialog/>
             <DropdownMenu modal={false}>
                 <DropdownMenuTrigger asChild>
                     {children}
@@ -26,7 +47,6 @@ export const TaskAction = ({
                 <DropdownMenuContent align="end" className="w-48">
                     <DropdownMenuItem
                         onClick={() => {}}
-                        disabled={false}
                         className="font-medium p-[10px]"
                     >
                         <ExternalLinkIcon className="size-4 mr-2 stroke-2"/>
@@ -35,7 +55,6 @@ export const TaskAction = ({
                                         
                     <DropdownMenuItem
                         onClick={() => {}}
-                        disabled={false}
                         className="font-medium p-[10px]"
                     >
                         <ExternalLinkIcon className="size-4 mr-2 stroke-2"/>
@@ -44,7 +63,6 @@ export const TaskAction = ({
 
                     <DropdownMenuItem
                         onClick={() => {}}
-                        disabled={false}
                         className="font-medium p-[10px]"
                     >
                         <PencilIcon className="size-4 mr-2 stroke-2"/>
@@ -52,8 +70,8 @@ export const TaskAction = ({
                     </DropdownMenuItem>
                     
                     <DropdownMenuItem
-                        onClick={() => {}}
-                        disabled={false}
+                        onClick={onDelete}
+                        disabled={isPending}
                         className="text-amber-700 focus:text-amber-700 font-medium p-[10px]"
                     >
                         <TrashIcon className="size-4 mr-2 stroke-2"/>
