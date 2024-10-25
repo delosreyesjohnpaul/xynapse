4e781cb5fb9f5e27a8ef2293f90e47db0681285c
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-25 19:50:54 +0800

    asldfh

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,6 +1,17 @@
 "use client";
 
+import { PageLoader } from "@/components/page-loader";
+import { useGetTask } from "@/features/tasks/api/use-get-task";
+import { useTaskId } from "@/features/tasks/hooks/use-task-id";
+
 export const TaskIdClient = () => {
+    const taskId = useTaskId();
+    const { data, isLoading } = useGetTask({ taskId });
+
+    if (isLoading) {
+        return <PageLoader/>
+    }
+
     return (
         <p>TaskIdClient</p>
     );
