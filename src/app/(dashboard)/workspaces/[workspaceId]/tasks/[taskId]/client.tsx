b6d28ece68f03d396c77d746573d54cc759d0675
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-25 20:39:01 +0800

    TaskOverview

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { DottedSeparator } from "@/components/dotted-separator";
 import { PageError } from "@/components/page-error";
 import { PageLoader } from "@/components/page-loader";
 import { useGetTask } from "@/features/tasks/api/use-get-task";
 import { TaskBreadcrumbs } from "@/features/tasks/components/task-breadcrumbs";
+import { TaskOverview } from "@/features/tasks/components/task-overview";
 import { useTaskId } from "@/features/tasks/hooks/use-task-id";
 
 export const TaskIdClient = () => {
@@ -21,6 +23,10 @@ export const TaskIdClient = () => {
     return (
         <div className="flex flex-col">
             <TaskBreadcrumbs project={data.project} task={data}/>
+            <DottedSeparator classname="my-6"/>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                <TaskOverview task={data}/>
+            </div>
         </div>
     );
 };
---
+++ src/features/tasks/components/task-overview.tsx
@@ -0,0 +1,13 @@
+import { Task } from "../types";
+
+interface TaskOverviewProps {
+    task: Task;
+};
+
+export const TaskOverview = ({ task } : TaskOverviewProps) => {
+    return (
+        <div className="flex flex-col gap-y-4 col-span-1">
+
+        </div>
+    )
+}
