87b76e4ff4e9907c430a555f2a416f06a60d8c78
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-25 13:33:42 +0800

    alsjfh

--- src/features/tasks/components/kanban-column-header.tsx
+++ src/features/tasks/components/kanban-column-header.tsx
@@ -1,26 +1,61 @@
 import { snakeCaseToTitleCase } from "@/lib/utils";
 import { TaskStatus } from "../types";
+import {
+    CircleCheckIcon,
+    CircleDashedIcon,
+    CircleDotDashedIcon,
+    CircleDotIcon,
+    CircleIcon,
+    PlusIcon,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 interface KanbanColumnHeaderProps {
     board: TaskStatus;
     taskCount: number;
 };
 
+
+const statusIconMap: Record<TaskStatus, React.ReactNode> = {
+    [TaskStatus.BACKLOG]: (
+        <CircleDashedIcon className="size-[18px] text-pink-400"/>
+    ),
+    [TaskStatus.TODO]: (
+        <CircleIcon className="size-[18px] text-red-400"/>
+    ),
+    [TaskStatus.IN_PROGRESS]: (
+        <CircleDotDashedIcon className="size-[18px] text-yellow-400"/>
+    ),
+    [TaskStatus.IN_REVIEW]: (
+        <CircleDotIcon className="size-[18px] text-blue-400"/>
+    ),
+    [TaskStatus.DONE]: (
+        <CircleCheckIcon className="size-[18px] text-emerald-400"/>
+    ),
+
+}
+
 export const KanbanColumnHeader = ({
     board,
     taskCount,
 } : KanbanColumnHeaderProps) => {
+    const icon = statusIconMap[board];
+
     return (
         <div className="px-2 py-1.5 flex items-center justify-between ">
             <div className="flex items-center gap-x-2">
-                <h2>
+                {icon}
+                <h2 className="text-sm font-medium">
                     {snakeCaseToTitleCase(board)}
                 </h2>
 
-                <div>
+                <div className="size-5 flex items-center justify-center rounded-md bg-neutral-200 text-xs text-neutral-700 font-medium">
                     {taskCount}
                 </div>
             </div>
+            <Button onClick={() => {}} variant="ghost" size="icon" className="size-5">
+                <PlusIcon className="size-4 text-neutral-500"/>
+            </Button>
         </div>
     )
 }
