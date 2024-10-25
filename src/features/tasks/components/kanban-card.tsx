e0d426eab0e443073b06e51f94b23a333be7b610
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-25 14:19:29 +0800

    alsfdjhs

--- src/features/tasks/components/kanban-card.tsx
+++ src/features/tasks/components/kanban-card.tsx
@@ -1,6 +1,9 @@
 import { MoreHorizontal } from "lucide-react";
 import { Task } from "../types";
 import { TaskAction } from "./task-actions";
+import { DottedSeparator } from "@/components/dotted-separator";
+import { MemberAvatar } from "@/features/members/components/member-avatar";
+import { TaskDate } from "./task-date";
 
 interface KanbanCardProps {
     task: Task;
@@ -14,6 +17,16 @@ export const KanbanCard = ({ task } : KanbanCardProps) => {
                 <TaskAction id={task.$id} projectId={task.projectId}>
                     <MoreHorizontal className="size-[18px] stroke-1 shrink-0 text-neutral-700 hover:opacity-75 transition"/>
                 </TaskAction>
+            </div>
+            <DottedSeparator/>
+
+            <div className="flex items-center gap-x-1.5 ">
+                <MemberAvatar
+                    name={task.assignee.name}
+                    fallbackClassName="text-[10px]"
+                />
+                <div className="size-1 rounded-full bg-neutral-300"/>
+                <TaskDate value={task.dueDate} className="text-xs"/>
             </div>
         </div>
     )
