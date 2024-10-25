bac01d6c99bd5b74059b7ff0059507375be2471e
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-25 14:23:38 +0800

    alkajdfh

--- src/features/tasks/components/kanban-card.tsx
+++ src/features/tasks/components/kanban-card.tsx
@@ -4,6 +4,7 @@ import { TaskAction } from "./task-actions";
 import { DottedSeparator } from "@/components/dotted-separator";
 import { MemberAvatar } from "@/features/members/components/member-avatar";
 import { TaskDate } from "./task-date";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 
 interface KanbanCardProps {
     task: Task;
@@ -27,6 +28,14 @@ export const KanbanCard = ({ task } : KanbanCardProps) => {
                 />
                 <div className="size-1 rounded-full bg-neutral-300"/>
                 <TaskDate value={task.dueDate} className="text-xs"/>
+            </div>
+            <div className="flex items-center gap-x-1.5 ">
+                <ProjectAvatar 
+                    name={task.project.name}
+                    image={task.project.imageUrl}
+                    fallbackClassName="text-[10px]"
+                />
+                <span className="text-xs font-medium">{task.project.name}</span>
             </div>
         </div>
     )
