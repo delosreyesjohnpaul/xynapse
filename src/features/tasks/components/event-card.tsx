659bb91b3e767e904de70da35d196064e021bba3
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-25 18:47:29 +0800

    laf

--- src/features/tasks/components/event-card.tsx
+++ src/features/tasks/components/event-card.tsx
@@ -1,6 +1,8 @@
 import { Project } from "@/features/projects/types";
 import { TaskStatus } from "../types";
 import { cn } from "@/lib/utils";
+import { MemberAvatar } from "@/features/members/components/member-avatar";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 
 interface EventCardProps {
     title: string;
@@ -16,8 +18,7 @@ const statusColorMap: Record<TaskStatus, string> = {
     [TaskStatus.IN_PROGRESS]: "border-l-yellow-500",
     [TaskStatus.IN_REVIEW]: "border-l-blue-500",
     [TaskStatus.DONE]: "border-l-emerald-500",
-
-}
+};
 
 export const EventCard = ({
     title,
@@ -29,9 +30,20 @@ export const EventCard = ({
     return (
         <div className="px-2 ">
             <div className={cn(
-                "p-1.5 text-xs bg-white text-primary border rounded-md border-l-4 flex flex-col gap-y-1.5 cursor-pointer hover:opacity-75 transition"
+                "p-1.5 text-xs bg-white text-primary border rounded-md border-l-4 flex flex-col gap-y-1.5 cursor-pointer hover:opacity-75 transition",
+                statusColorMap[status] 
             )}>
                 <p>{title}</p>
+                <div className="flex items-center gap-x-1">
+                    <MemberAvatar
+                        name={assignee?.name}
+                    />
+                    <div className="size-1 rounded-full bg-neutral-300"/>
+                    <ProjectAvatar
+                        name={project?.name}
+                        image={project?.imageUrl}
+                    />
+                </div>
             </div>
         </div>
     )
