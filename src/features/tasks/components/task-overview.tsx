2c59e0d4cf11fd2ab542ad34d12ec5a87e7f7b17
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-25 20:45:27 +0800

    slfaklf

--- src/features/tasks/components/task-overview.tsx
+++ src/features/tasks/components/task-overview.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { Task } from "../types";
+import { PencilIcon } from "lucide-react";
+import { DottedSeparator } from "@/components/dotted-separator";
 
 interface TaskOverviewProps {
     task: Task;
@@ -7,7 +10,20 @@ interface TaskOverviewProps {
 export const TaskOverview = ({ task } : TaskOverviewProps) => {
     return (
         <div className="flex flex-col gap-y-4 col-span-1">
+            <div className="bg-muted rounded-lg p-4">
+                <div className="flex items-center justify-between">
+                    <p className="text-lg font-semibold">Overview</p>
 
-        </div>
+                    <Button size="sm" variant="secondary">
+                        <PencilIcon className="size-4 mr-2"/>\
+                        Edit
+                    </Button>
+                </div>
+                <DottedSeparator classname="my-4"/>
+                <div className="flex flex-col gap-y-4">
+                    
+                </div>
+            </div>
+        </div> 
     )
 }
