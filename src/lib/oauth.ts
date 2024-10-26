58c8d12831e2f71cd74cda2dbce0d383e9c68bca
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-26 21:33:54 +0800

    bug lol

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -22,12 +22,12 @@ export async function signUpWithGithub() {
 export async function signUpWithGoogle() {
 	const { account } = await createAdminClient();
 
-//   const origin = headers().get("origin");
+  const origin = headers().get("origin");
   
 	const redirectUrl = await account.createOAuth2Token(
 		OAuthProvider.Google,
-		`${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
-		`${process.env.NEXT_PUBLIC_APP_URL}/sign-up`,
+		`${origin}/oauth`,
+		`${origin}/sign-up`,
 	);
 
 	return redirect(redirectUrl);
