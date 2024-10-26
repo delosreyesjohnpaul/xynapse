2e48659c6d364e39ff4a05ce515c3e7bb0b1e686
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-26 19:40:50 +0800

    alshdjf

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,39 @@
+"use client";
+
 import { UserButton } from "@/features/auth/components/user-button";
 import { MobileSidebar } from "./mobile-sidebar";
+import { usePathname } from "next/navigation";
+
+const pathNameMap = {
+    "tasks": {
+        title: "My Tasks",
+        description: "View all your tasks here.",
+    },
+
+    "projects": {
+        title: "My Projects",
+        description: "View your project tasks here.",
+    },
+}
+
+const defaultMap = {
+    title: "Home",
+    description: "Track and manage all your projects and tasks in one place.",
+};
 
 export const Navbar = () => {
+    const pathname = usePathname();
+    const pathnameParts = pathname.split("/");
+
+    const pathnameKey = pathnameParts[3] as keyof typeof pathNameMap;
+
+    const { title, description } = pathNameMap[pathnameKey] || defaultMap;
 
     return (
        <nav className="pt-4 px-6 flex items-center justify-between">
         <div className="flex-col hidden lg:flex ">
-            <h1 className="text-2xl font-semibold">Home</h1>
-            <p className="text-muted-foreground ">Track and manage all your projects and tasks in one place.</p>
+            <h1 className="text-2xl font-semibold">{title}</h1>
+            <p className="text-muted-foreground ">{description}</p>
         </div>
         <MobileSidebar/>
         <UserButton/>
